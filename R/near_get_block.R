ecc65dedec85df71e0b078f4be0a97ed438e3d21
Saw Simeon <doe_doe207@hotmail.com>
2025-12-07 20:16:40 +0100

    change from two argument to one argument to the function

--- R/near_get_block.R
+++ R/near_get_block.R
@@ -1,35 +1,32 @@
-#' Get a block from NEAR
+#' Get Block Information by Height or Hash
 #'
-#' Retrieve a block using the `block` method. Provide either `finality` or `block_id`.
+#' @param block_id Block height (integer), hash (character), or `"final"` (latest finalized).
+#' @return A tibble with block header and chunk info.
 #'
-#' @param finality Character scalar. One of "final" or "optimistic". Defaults to "final".
-#' @param block_id Character or integer. Block height (integer) or block hash (string).
-#' @return A tibble with header (as a list-column) and chunks (list-column). Header fields are available inside header.
+#' @export
+#'
 #' @examples
 #' \dontrun{
-#' near_set_endpoint("https://rpc.testnet.near.org")
-#' near_get_block(finality = "final")
-#' near_get_block(block_id = 1234567)
+#' near_get_block("final")
+#' near_get_block(123456789)
+#' near_get_block("8Yd8...abc")
 #' }
-#' @seealso
-#'  URL{https://docs.near.org/api/rpc/blocks}
-#' @export
-near_get_block <- function(finality = "final", block_id = NULL) {
-  if (!is.null(block_id) && !missing(finality)) {
-    rlang::abort("Specify either finality or block_id, not both")
-  }
+#'
+#' @seealso \url{https://docs.near.org/api/rpc/blocks}
+#'
+near_get_block <- function(block_id = "final") {
+  params <- list(block_id = block_id)
+  resp <- near_rpc("block", params)
 
-  params <- list()
-  if (!is.null(block_id)) params$block_id <- block_id else params$finality <- finality
-
-  resp <- near_rpc("block", params = params)
-  res <- if (!is.null(resp$result)) resp$result else resp
-
-  if (!is.list(res)) rlang::abort("Unexpected response format from RPC")
-
-  t <- tibble::tibble(
-    header = list(res$header %||% list()),
-    chunks = list(res$chunks %||% list())
+  res <- resp$result %||% resp
+  tibble::tibble(
+    height        = res$header$height %||% NA_integer_,
+    hash          = res$header$hash %||% NA_character_,
+    prev_hash     = res$header$prev_hash %||% NA_character_,
+    timestamp     = as.POSIXct(res$header$timestamp / 1e9, origin = "1970-01-01"),
+    author        = res$author %||% NA_character_,
+    chunks_count  = length(res$chunks),
+    gas_price     = res$header$gas_price %||% NA_character_,
+    raw_response  = list(resp)
   )
-  return(t)
 }
