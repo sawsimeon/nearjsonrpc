1ee9fe3405542bd6c118265ac93f58857f16e3f4
Saw Simeon <doe_doe207@hotmail.com>
2025-12-07 11:44:07 +0100

    added mocked test code for near_broadcast_tx.R

--- tests/testthat/test-near_broadcast_tx.R
+++ tests/testthat/test-near_broadcast_tx.R
@@ -7,12 +7,12 @@ withr::local_options(list(nearjsonrpc.rpc_fn = function(method, params, timeout)
   if (params$wait_until == "NONE") mock_resp_hash else mock_resp_full
 }))
 
-test_that("near_broadcast_tx returns hash when NONE", {
+test_that("near_broadcast_tx returns hash while NONE", {
   res <- near_broadcast_tx("B64TX", wait_until = "NONE")
   expect_equal(res$hash, "txhash123")
 })
 
-test_that("near_broadcast_tx returns detailed when waited", {
+test_that("near_broadcast_tx returns detailed while waited", {
   res <- near_broadcast_tx("B64TX", wait_until = "FINAL")
   expect_s3_class(res, "tbl_df")
   expect_equal(res$hash, "txhash123")
